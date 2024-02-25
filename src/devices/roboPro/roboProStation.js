b270a7789bb9b88a3adde0660f457b79aae1a4a1
s.gologuzov <s.gologuzov@keysoftware.ru>
2024-02-25 18:54:19 +0300

    Added Robo Station blocks.

--- src/devices/roboPro/roboProStation.js
+++ src/devices/roboPro/roboProStation.js
@@ -5,10 +5,15 @@
  * use to uart chip, uese oldbootloader to flash the firmware, and there are
  * more A6 and A7 pin options.
  */
-const OpenBlockArduinoUnoDevice = require('../arduinoUno/arduinoUno');
 
 const ArduinoPeripheral = require('../common/arduino-peripheral');
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const Cast = require('../../util/cast');
+const OpenBlockArduinoUnoDevice = require('../arduinoUno/arduinoUno');
 const formatMessage = require('format-message');
+const log = require('../../util/log');
+
 
 /**
  * The list of USB device filters.
@@ -39,12 +44,61 @@ const DIVECE_OPT = {
     firmware: 'arduinoUno.hex'
 };
 
+const Pins = {
+    D0: '0',
+    D1: '1',
+    D2: '2',
+    D3: '3',
+    D4: '4',
+    D5: '5',
+    D6: '6',
+    D7: '7',
+    D8: '8',
+    D9: '9',
+    D10: '10',
+    D11: '11',
+    D12: '12',
+    D13: '13',
+    A0: 'A0',
+    A1: 'A1',
+    A2: 'A2',
+    A3: 'A3',
+    A4: 'A4',
+    A5: 'A5'
+};
+
+const Level = {
+    High: 'HIGH',
+    Low: 'LOW'
+};
+
+const PinsMap = {
+    // RoboPro
+    DataLED: Pins.D2,
+    Speaker: Pins.D3,
+    ClkLED: Pins.D4,
+    GreenLED: Pins.D5,
+    YellowLED: Pins.D6,
+    RedLED: Pins.D7,
+    Button1: Pins.D8,
+    Button2: Pins.D9,
+    Button3: Pins.D10,
+    Button5: Pins.D11,
+    Button4: Pins.D12,
+    ExtSensor1: Pins.D13,
+    ExtSensor3: Pins.A0,
+    ExtSensor2: Pins.A1,
+    Slider: Pins.A2,
+    Microphone: Pins.A3,
+    LightSensor: Pins.A4
+};
+
 /**
- * Manage communication with a Arduino Nano peripheral over a OpenBlock Link client socket.
+ * Manage communication with an Arduino Nano peripheral over a OpenBlock Link client socket.
  */
-class RoboProStation extends ArduinoPeripheral{
+class RoboProStation extends ArduinoPeripheral {
     /**
-     * Construct a Arduino communication object.
+     * Construct an Arduino communication object.
      * @param {Runtime} runtime - the OpenBlock runtime
      * @param {string} deviceId - the id of the extension
      * @param {string} originalDeviceId - the original id of the peripheral, like xxx_arduinoUno
@@ -55,15 +109,81 @@ class RoboProStation extends ArduinoPeripheral{
 }
 
 /**
-  * OpenBlock blocks to interact with a Arduino Nano Ultra peripheral.
-  */
-class OpenBlockRoboProStationDevice extends OpenBlockArduinoUnoDevice{
+ * OpenBlock blocks to interact with an Arduino Nano Ultra peripheral.
+ */
+class OpenBlockRoboProStationDevice extends OpenBlockArduinoUnoDevice {
 
     /**
-      * @return {string} - the ID of this extension.
-      */
+     * @return {string} - the ID of this extension.
+     */
     get DEVICE_ID () {
         return 'roboProStation';
+    }
+
+    get LEDS_MENU () {
+        return [
+            {
+                text: '0',
+                value: 0
+            },
+            {
+                text: '1',
+                value: 1
+            },
+            {
+                text: '2',
+                value: 2
+            },
+            {
+                text: '3',
+                value: 3
+            },
+            {
+                text: '4',
+                value: 4
+            },
+            {
+                text: '5',
+                value: 5
+            },
+            {
+                text: '6',
+                value: 6
+            },
+            {
+                text: '7',
+                value: 7
+            }
+        ];
+    }
+
+    get COLOR_LEDS_MENU () {
+        return [
+            {
+                text: formatMessage({
+                    id: 'roboPro.colorLedsMenu.red',
+                    default: 'red',
+                    description: 'label for red LED'
+                }),
+                value: PinsMap.RedLED
+            },
+            {
+                text: formatMessage({
+                    id: 'roboPro.colorLedsMenu.yellow',
+                    default: 'yellow',
+                    description: 'label for yellow LED'
+                }),
+                value: PinsMap.YellowLED
+            },
+            {
+                text: formatMessage({
+                    id: 'roboPro.colorLedsMenu.green',
+                    default: 'green',
+                    description: 'label for green LED'
+                }),
+                value: PinsMap.GreenLED
+            }
+        ];
     }
 
     /**
@@ -90,13 +210,164 @@ class OpenBlockRoboProStationDevice extends OpenBlockArduinoUnoDevice{
                     default: 'Robo Station',
                     description: 'The name of the arduino uno device pin category'
                 }),
-                color1: '#4C97FF',
-                color2: '#3373CC',
-                color3: '#3373CC',
+                color1: '#989898',
+                color2: '#989898',
+                color3: '#000000',
 
-                blocks: []
+                blocks: [
+                    {
+                        opcode: 'ledTurnOn',
+                        text: formatMessage({
+                            id: 'roboPro.station.ledTurnOn',
+                            default: 'Turn LED [LED_INDEX] on',
+                            description: 'Turn LED on'
+                        }),
+                        blockType: BlockType.COMMAND,
+                        arguments: {
+                            LED_INDEX: {
+                                type: ArgumentType.NUMBER,
+                                menu: 'leds',
+                                defaultValue: 0
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'ledTurnOff',
+                        text: formatMessage({
+                            id: 'roboPro.station.ledTurnOff',
+                            default: 'Turn LED [LED_INDEX] off',
+                            description: 'Turn LED off'
+                        }),
+                        blockType: BlockType.COMMAND,
+                        arguments: {
+                            LED_INDEX: {
+                                type: ArgumentType.NUMBER,
+                                menu: 'leds',
+                                defaultValue: 0
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'colorLedTurnOn',
+                        text: formatMessage({
+                            id: 'roboPro.station.colorLedTurnOn',
+                            default: 'Turn LED [LED_PIN] on',
+                            description: 'Turn LED on'
+                        }),
+                        blockType: BlockType.COMMAND,
+                        arguments: {
+                            LED_PIN: {
+                                type: ArgumentType.STRING,
+                                menu: 'colorLeds',
+                                defaultValue: PinsMap.RedLED
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'colorLedTurnOff',
+                        text: formatMessage({
+                            id: 'roboPro.station.colorLedTurnOff',
+                            default: 'Turn LED [LED_PIN] on',
+                            description: 'Turn LED on'
+                        }),
+                        blockType: BlockType.COMMAND,
+                        arguments: {
+                            LED_PIN: {
+                                type: ArgumentType.STRING,
+                                menu: 'colorLeds',
+                                defaultValue: PinsMap.RedLED
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'colorLedTurnOff',
+                        text: formatMessage({
+                            id: 'roboPro.station.colorLedTurnOff',
+                            default: 'Turn LED [LED_PIN] on',
+                            description: 'Turn LED on'
+                        }),
+                        blockType: BlockType.COMMAND,
+                        arguments: {
+                            LED_PIN: {
+                                type: ArgumentType.STRING,
+                                menu: 'colorLeds',
+                                defaultValue: PinsMap.RedLED
+                            }
+                        }
+                    },
+                    {
+                        opcode: 'playNote',
+                        text: formatMessage({
+                            id: 'roboPro.station.playNote',
+                            default: 'Play note [NOTE]',
+                            description: 'play a note'
+                        }),
+                        blockType: BlockType.COMMAND,
+                        arguments: {
+                            NOTE: {
+                                type: ArgumentType.NOTE,
+                                defaultValue: 48
+                            }
+                        }
+                    }
+                ],
+                menus: {
+                    leds: {
+                        items: this.LEDS_MENU
+                    },
+                    colorLeds: {
+                        items: this.COLOR_LEDS_MENU
+                    }
+                }
             }
         ];
+    }
+
+    /**
+     * Turn on LED.
+     * @param {object} args - the block's arguments.
+     */
+    ledTurnOn (args) {
+        const ledIndex = args.LED_INDEX;
+        log.info(`[ledTurnOn] ledIndex: ${ledIndex}`);
+    }
+
+    /**
+     * Turn off LED.
+     * @param {object} args - the block's arguments.
+     */
+    ledTurnOff (args) {
+        const ledIndex = args.LED_INDEX;
+        log.info(`[ledTurnOff] ledIndex: ${ledIndex}`);
+    }
+
+    /**
+     * Turn on color LED.
+     * @param {object} args - the block's arguments.
+     */
+    colorLedTurnOn (args) {
+        const ledPin = args.LED_PIN;
+        log.info(`[colorLedTurnOn] ledPin: ${ledPin}`);
+        this._peripheral.setDigitalOutput(ledPin, Level.High);
+    }
+
+    /**
+     * Turn off color LED.
+     * @param {object} args - the block's arguments.
+     */
+    colorLedTurnOff (args) {
+        const ledPin = args.LED_PIN;
+        log.info(`[colorLedTurnOff] ledPin: ${ledPin}`);
+        this._peripheral.setDigitalOutput(ledPin, Level.Low);
+    }
+
+    /**
+     * Play a note.
+     * @param {object} args - the block's arguments.
+     */
+    playNote (args) {
+        const note = Cast.toNumber(args.NOTE);
+        log.info(`[playNote] note: ${note}`);
     }
 }
 
