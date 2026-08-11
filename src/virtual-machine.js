704117f9619caa8d7ed552bf53064bb419641343
s.gologuzov <s.gologuzov@keysoftware.ru>
2026-08-11 16:58:44 +0300

    Проект с блоками РобоБота не открывается после выбора файла из другой версии ПО

--- src/virtual-machine.js
+++ src/virtual-machine.js
@@ -23,6 +23,7 @@ const newBlockIds = require('./util/new-block-ids');
 const {loadCostume} = require('./import/load-costume.js');
 const {loadSound} = require('./import/load-sound.js');
 const {serializeSounds, serializeCostumes} = require('./serialization/serialize-assets');
+const {preprocessProjectInput} = require('./util/normalize-project');
 require('canvas-toBlob');
 
 const RESERVED_NAMES = ['_mouse_', '_stage_', '_edge_', '_myself_', '_random_'];
@@ -416,37 +417,43 @@ class VirtualMachine extends EventEmitter {
             input = JSON.stringify(input);
         }
 
-        const validationPromise = new Promise((resolve, reject) => {
-            const validate = require('scratch-parser');
-            // The second argument of false below indicates to the validator that the
-            // input should be parsed/validated as an entire project (and not a single sprite)
-            validate(input, false, (error, res) => {
-                if (error) return reject(error);
-                resolve(res);
-            });
-        })
-            .catch(error => {
-                const {SB1File, ValidationError} = require('scratch-sb1-converter');
+        // Preprocess the input to normalize shorthand CSS colors before
+        // the scratch-parser schema validation rejects them.
+        const preprocessedPromise = preprocessProjectInput(input);
 
-                try {
-                    const sb1 = new SB1File(input);
-                    const json = sb1.json;
-                    json.projectVersion = 2;
-                    return Promise.resolve([json, sb1.zip]);
-                } catch (sb1Error) {
-                    if (sb1Error instanceof ValidationError) {
-                        // The input does not validate as a Scratch 1 file.
-                    } else {
-                        // The project appears to be a Scratch 1 file but it
-                        // could not be successfully translated into a Scratch 2
-                        // project.
-                        return Promise.reject(sb1Error);
+        const validationPromise = preprocessedPromise.then(preprocessedInput =>
+            new Promise((resolve, reject) => {
+                const validate = require('scratch-parser');
+                // The second argument of false below indicates to the validator that the
+                // input should be parsed/validated as an entire project (and not a single sprite)
+                validate(preprocessedInput, false, (error, res) => {
+                    if (error) return reject(error);
+                    resolve(res);
+                });
+            })
+                .catch(error => {
+                    const {SB1File, ValidationError} = require('scratch-sb1-converter');
+
+                    try {
+                        const sb1 = new SB1File(input);
+                        const json = sb1.json;
+                        json.projectVersion = 2;
+                        return Promise.resolve([json, sb1.zip]);
+                    } catch (sb1Error) {
+                        if (sb1Error instanceof ValidationError) {
+                            // The input does not validate as a Scratch 1 file.
+                        } else {
+                            // The project appears to be a Scratch 1 file but it
+                            // could not be successfully translated into a Scratch 2
+                            // project.
+                            return Promise.reject(sb1Error);
+                        }
                     }
-                }
-                // Throw original error since the input does not appear to be
-                // an SB1File.
-                return Promise.reject(error);
-            });
+                    // Throw original error since the input does not appear to be
+                    // an SB1File.
+                    return Promise.reject(error);
+                })
+        );
 
         return validationPromise
             .then(validatedInput => this.deserializeProject(validatedInput[0], validatedInput[1]))
@@ -783,16 +790,21 @@ class VirtualMachine extends EventEmitter {
             input = JSON.stringify(input);
         }
 
-        const validationPromise = new Promise((resolve, reject) => {
-            const validate = require('scratch-parser');
-            // The second argument of true below indicates to the parser/validator
-            // that the given input should be treated as a single sprite and not
-            // an entire project
-            validate(input, true, (error, res) => {
-                if (error) return reject(error);
-                resolve(res);
-            });
-        });
+        // Preprocess to normalize shorthand CSS colors before validation.
+        const preprocessedPromise = preprocessProjectInput(input, true);
+
+        const validationPromise = preprocessedPromise.then(preprocessedInput =>
+            new Promise((resolve, reject) => {
+                const validate = require('scratch-parser');
+                // The second argument of true below indicates to the parser/validator
+                // that the given input should be treated as a single sprite and not
+                // an entire project
+                validate(preprocessedInput, true, (error, res) => {
+                    if (error) return reject(error);
+                    resolve(res);
+                });
+            })
+        );
 
         return validationPromise
             .then(validatedInput => {
